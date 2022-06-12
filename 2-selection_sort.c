470d82530f4f1429f1028d886c58556e196aa437
gerardomp18 <gerardomarinparra18@gmail.com>
2022-06-11 22:28:04 -0500

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: list
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t minimo;
+	size_t x;
+	size_t z;
+	size_t temp;
+
+	if ((array != NULL) && (array >= 2))
+	{
+		for (x = 0; x < size; x++)
+		{
+			minimo = x;
+			for (z = x + 1; z < size; z++)
+			{
+				if (array[z] < array[minimo])
+				{
+					minimo = z;
+				}
+			}
+			if (minimo != x)
+			{
+				temp = array[x];
+				array[x] = array[minimo];
+				array[minimo] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
